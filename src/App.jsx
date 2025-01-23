119e425ed8a2b824a9268454020cda087d34ef9b
Mr.Shahzad.Developer <mr.shahzad.developer@gmail.com>
2025-01-23 23:02:56 +0500

    The Last Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css'
+import About from './Components/About'
+import Contact from './Components/Contact'
+import Features from './Components/Features'
+import Footer from './Components/Footer'
 import Hero from './Components/Hero'
+import Navbar from './Components/Navbar'
+import FloatingImage from './Components/Story'
 
 function App() {
 
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'> 
+    <Navbar />
      <Hero />
+     <About />
+     <Features />
+     <FloatingImage />
+     <Contact />
+     <Footer />
     </main>
   )
 }
